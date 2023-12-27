c8a3dc24c3b2d666f958151863bb7553bb035785
Pakawat Anekwiroj <pakawat.ane@gmail.com>
2023-12-27 23:47:27 +0700

    update UI and add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,40 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'ระบบ e-Tax Invoice / e-Receipt',
-  description: 'ค้นหารายชื่อผู้ประกอบการที่ได้รับอนุมัติให้จัดทำ ส่งมอบ และเก็บรักษาใบกำกับภาษีอิเล็กทรอนิกส์ และใบรับอิเล็กทรอนิกส์',
-}
+  title: "ระบบ e-Tax Invoice / e-Receipt",
+  description:
+    "ค้นหารายชื่อผู้ประกอบการที่ได้รับอนุมัติให้จัดทำ ส่งมอบ และเก็บรักษาใบกำกับภาษีอิเล็กทรอนิกส์ และใบรับอิเล็กทรอนิกส์",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <footer className="text-center mb-8">
+        <div>
+          <div>© 2023, Crafted with 💖 By HSpotlight</div>
+          <div>
+            Facebook:{" "}
+            <a href="https://www.facebook.com/HSpotlight/" target="_blank">
+              HSpotlight
+            </a>
+          </div>
+          <div>
+            Github:{" "}
+            <a href="https://www.github.com/hspotlight/" target="_blank">
+              HSpotlight
+            </a>
+          </div>
+        </div>
+      </footer>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import SearchSection from './SearchSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between px-24 pt-24 pb-12">
       <SearchSection></SearchSection>
     </main>
   )

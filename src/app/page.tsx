79ac6fd0a5253787734741f34a9b8de704b3ae5b
Pakawat Anekwiroj <pakawat.ane@gmail.com>
2023-12-28 08:14:52 +0700

    fix UI a bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import SearchSection from './SearchSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24 pt-24 pb-12">
+    <main className="flex flex-col items-center justify-between px-24 pt-24 pb-12" style={{minHeight: 'calc(100vh - 128px)'}}>
       <SearchSection></SearchSection>
     </main>
   )
